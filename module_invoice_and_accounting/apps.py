34697ea0a95a92b7eb2afd2063a40e61eac814f7
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 23:28:02 +0100

    Ajout de l'application module_invoice_and_accounting

---
+++ module_invoice_and_accounting/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModuleInvoiceAndAccountingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'module_invoice_and_accounting'
