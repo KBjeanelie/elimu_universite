62c35e73626e42a15b8b180bcd04857d931b8413
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 17:08:43 +0100

    MAJ

--- school_management/admin.py
+++ school_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from school_management.models import AdemicYear
+from school_management.models import AdemicYear, Level
 
 # Register your models here.
 admin.site.register(AdemicYear)
+admin.site.register(Level)
