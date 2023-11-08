714ba66bcdf3e9414cec9f98f469e09059b42ee9
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 22:07:54 +0100

    - Create model ReportCard

--- module_assessments/admin.py
+++ module_assessments/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from module_assessments.models import Assessment, TypeOfEvaluation
+from module_assessments.models import Assessment, ReportCard, TypeOfEvaluation
 
 # Register your models here.
 admin.site.register(TypeOfEvaluation)
 admin.site.register(Assessment)
+admin.site.register(ReportCard)
--- module_assessments/models.py
+++ module_assessments/models.py
@@ -12,13 +12,23 @@ class TypeOfEvaluation(models.Model):
         return f"Sector: {self.title}"
 
 
-
 class Assessment(models.Model):
     note = models.IntegerField(default=0)
     is_publish = models.BooleanField(default=False)
     type_evaluation = models.ForeignKey(TypeOfEvaluation, on_delete=models.SET_NULL, null=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+    career = models.ForeignKey(Career, on_delete=models.DO_NOTHING)
+    semester = models.ForeignKey(Semester, on_delete=models.DO_NOTHING)
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ReportCard(models.Model):
+    note = models.IntegerField(default=0)
+    file = models.FileField(upload_to='bulletins')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     career = models.ForeignKey(Career, on_delete=models.DO_NOTHING)
     semester = models.ForeignKey(Semester, on_delete=models.DO_NOTHING)
     academic_year = models.ForeignKey(AcademicYear, on_delete=models.DO_NOTHING)
