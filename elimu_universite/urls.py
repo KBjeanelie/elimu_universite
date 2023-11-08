1d4ca2dcae17cab3f9d6e3936af7eec186b9a745
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 09:33:14 +0100

    Initialisation du projet et creation du model User

---
+++ elimu_universite/urls.py
@@ -0,0 +1,8 @@
+"""elimu_universite URL Configuration """
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
---
+++ elimu_universite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for elimu_universite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elimu_universite.settings')
+
+application = get_wsgi_application()
