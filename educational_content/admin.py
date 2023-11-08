71e38fc1868a90169610d118126853a4d2b39ee3
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 16:24:24 +0100

    MAJ

--- educational_content/admin.py
+++ educational_content/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from educational_content.models import File, Folder
+from educational_content.models import BookCategory, File, Folder
 
 # Register your models here.
 admin.site.register(Folder)
 admin.site.register(File)
+admin.site.register(BookCategory)
