00f25903d6f8f0230d4613b65c7c75a8ec144707
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 18:18:27 +0100

    ajout du model GroupSubject

--- school_management/admin.py
+++ school_management/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from school_management.models import AdemicYear, Document, DocumentType, Level, Program
+from school_management.models import AdemicYear, Document, DocumentType, GroupSubject, Level, Program
 
 # Register your models here.
 admin.site.register(AdemicYear)
@@ -8,3 +8,4 @@ admin.site.register(Level)
 admin.site.register(DocumentType)
 admin.site.register(Document)
 admin.site.register(Program)
+admin.site.register(GroupSubject)
