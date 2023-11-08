1d7d68e0bac7ed21c6a074fe3a6f78ae59500e33
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-08 21:13:02 +0100

    - Create model eBook     - Create model Book

--- educational_content/models.py
+++ educational_content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from school_management.models import Career, Sector
 
 from user_account.models import Teacher
 
@@ -44,21 +45,47 @@ class BookCategory(models.Model):
     def __str__(self):
         return f"Categorie Livre : {self.label}"
 
-# class Book(models.Model):
+class Book(models.Model):
     
-#     label = models.CharField(max_length=120)
+    label = models.CharField(max_length=120)
     
-#     author = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
     
-#     code_isbn = models.CharField(max_length=120)
+    code_isbn = models.CharField(max_length=120)
     
-#     location = models.CharField(max_length=120, blank=True, blank=True)
+    location = models.CharField(max_length=120, blank=True)
     
-#     attachement = models.FileField(upload_to='public_folder', blank=True)
+    attachement = models.FileField(upload_to='public_folder', blank=True)
     
-#     created_at = models.DateTimeField(auto_now_add=True)
+    sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, null=True, blank=True)
     
-#     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
-#     def __str__(self):
-#         return f"file : {self.label}"
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"file : {self.label}"
+    
+
+class eBook(models.Model):
+    
+    title = models.CharField(max_length=120)
+    
+    author = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+    
+    photo_cover = models.ImageField(upload_to='images_ebook')
+    
+    attachement = models.FileField(upload_to='ebook_folder')
+    
+    is_private = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"file : {self.title}"
