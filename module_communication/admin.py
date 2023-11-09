121b2af56358bc9f8a4132815832ad5dacbc58a1
Jean-Elie <jeaneliekubembula@gmail.com>
2023-11-09 02:11:50 +0100

    - Create model DiscussionGroup     - Create model Event     - Create model Group     - Create model Information     - Create model StudentDiscussionMedia     - Create model StudentDiscussion     - Create model GroupMessage     - Create model GroupMedia     - Create model EventParticipate     - Add field group to discussiongroup     - Add field student to discussiongroup     - Create model Contact

--- module_communication/admin.py
+++ module_communication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from module_communication.models import Contact, DiscussionGroup, Event, EventParticipate, Group, GroupMedia, GroupMessage, Information, StudentDiscussion, StudentDiscussionMedia
+
 # Register your models here.
+admin.site.register(Information)
+admin.site.register(Event)
+admin.site.register(EventParticipate)
+admin.site.register(Group)
+admin.site.register(DiscussionGroup)
+admin.site.register(GroupMessage)
+admin.site.register(GroupMedia)
+admin.site.register(Contact)
+admin.site.register(StudentDiscussion)
+admin.site.register(StudentDiscussionMedia)
--- module_communication/models.py
+++ module_communication/models.py
@@ -1,3 +1,141 @@
 from django.db import models
+from school_management.models import Career
+
+from user_account.models import Student
+from elimu_universite.constant import hours_of_the_day
 
 # Create your models here.
+class Information(models.Model):
+    
+    title = models.CharField(max_length=50)
+    
+    date_info = models.DateField()
+    
+    content = models.TextField(blank=True)
+    
+    file = models.FileField(upload_to='information_doc', blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Event(models.Model):
+    
+    title = models.CharField(max_length=50)
+    
+    start_date = models.DateField()
+    
+    end_date = models.DateField()
+    
+    content = models.TextField(blank=True)
+    
+    file = models.FileField(upload_to='information_doc', blank=True)
+    
+    photo = models.ImageField(upload_to='event_doc', blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class EventParticipate(models.Model):
+    
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
+    
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    
+    start_hours = models.CharField(max_length=20, choices=hours_of_the_day)
+    
+    end_hours = models.CharField(max_length=20, choices=hours_of_the_day)
+    
+    amount = models.FloatField(default=0)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Group(models.Model):
+    
+    title = models.CharField(max_length=20)
+    
+    career = models.ForeignKey(Career, on_delete=models.CASCADE,null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+class DiscussionGroup(models.Model):
+    
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class GroupMessage(models.Model):
+    
+    discussion_group = models.ForeignKey(DiscussionGroup, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class GroupMedia(models.Model):
+    
+    discussion_group = models.ForeignKey(DiscussionGroup, on_delete=models.CASCADE)
+    
+    legend = models.CharField(max_length=60, blank=True)
+    
+    file = models.FileField(upload_to='group_media')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Contact(models.Model):
+    
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="contacts")
+    
+    can_discuss_with = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="can_be_discussed_with")
+    
+    is_friend = models.BooleanField(default=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StudentDiscussion(models.Model):
+    
+    content = models.TextField()
+    
+    sender = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="sent_discussions")
+    
+    receiver = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="received_discussions")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StudentDiscussionMedia(models.Model):
+    
+    legend = models.TextField(blank=True)
+    
+    file = models.FileField(upload_to='student_media')
+    
+    sender = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="sent_media")
+    
+    receiver = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="received_media")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
