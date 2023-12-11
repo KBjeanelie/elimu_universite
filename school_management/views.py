cefa5d8df8c90f060560ab67bcf7b97ee6a03f6c
Jean-Elie <jeaneliekubembula@gmail.com>
2023-12-11 11:37:10 +0100

    MAJ et recuperation des modification de Schadrack

--- school_management/views.py
+++ school_management/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from school_management.models import AcademicYear, Career, Document, DocumentType, GroupSubject, Level, Program, SanctionAppreciation, SanctionAppreciationType, Schedule, Sector, Semester, StudentCareer, Subject
 from school_management.serializers import AcademicYearSerializer, CareerSerializer, DocumentSerializer, DocumentTypeSerializer, GroupSubjectSerializer, LevelSerializer, ProgramSerializer, SanctionAppreciationSerializer, SanctionAppreciationTypeSerializer, ScheduleSerializer, SectorSerializer, SemesterSerializer, StudentCareerSerializer, SubjectSerializer
+from django.shortcuts import get_object_or_404, render
+from.models import *
+from django.http import JsonResponse
+
 
 # Create your views here.
 class AcademicYearViewSet(viewsets.ModelViewSet):
@@ -58,3 +62,51 @@ class ScheduleViewSet(viewsets.ModelViewSet):
 class SanctionAppreciationViewSet(viewsets.ModelViewSet):
     queryset = SanctionAppreciation.objects.all()
     serializer_class = SanctionAppreciationSerializer
+
+
+def teacher_schedule(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    subjects_taught = teacher.subject_set.all()
+    
+    schedules = Schedule.objects.filter(subject__in=subjects_taught)
+    
+    context = {'teacher': teacher, 'schedules': schedules}
+    
+    return render(request, 'teacher_schedule.html', context)
+
+
+def teacher_documents(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    documents = Document.objects.filter(subject__teacher_in_charge=teacher)
+    
+    context = {'teacher': teacher, 'documents': documents}
+    
+    return render(request, 'teacher_documents.html', context)
+
+
+def total_sanctions(request):
+    total_sanctions_count = SanctionAppreciation.objects.count()
+    
+    return JsonResponse({'total_sanctions_count': total_sanctions_count})
+
+
+
+def all_career_schedules(request):
+    careers = Career.objects.all()
+    
+    schedules_by_career = {}
+    for career in careers:
+        schedules = Schedule.objects.filter(career=career)
+        schedules_by_career[career] = schedules
+    
+    context = {'schedules_by_career': schedules_by_career}
+    
+    return render(request, 'all_career_schedules.html', context)
+
+
+def students_and_careers(request, academic_year_id):
+    student_careers = StudentCareer.objects.filter(academic_year_id=academic_year_id)
+    
+    context = {'student_careers': student_careers}
+    
+    return render(request, 'students_and_careers.html', context)
