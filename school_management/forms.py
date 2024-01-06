eec4a21513076ce52d312c0101f73fd0250696e6
Jean-Elie <jeaneliekubembula@gmail.com>
2024-01-06 12:09:55 +0100

    MAJ des model de formulaire pour le module gestion universite

--- school_management/forms.py
+++ school_management/forms.py
@@ -20,21 +20,121 @@ class AcademicYearForm(forms.ModelForm):
     class Meta:
         model = AcademicYear
         fields = ['label', 'start_date', 'end_date', 'status']
+        widgets = {
+            'label' : forms.TextInput(
+                attrs={
+                    'id': 'label',
+                    'class': 'form-control',
+                    'name': 'label',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+            'start_date': forms.DateInput(
+                attrs={
+                    'type':'date',
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'end_date': forms.DateInput(
+                attrs={
+                    'type':'date',
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'status':forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input',
+                    'required': True,
+                }
+            )
+        }
 
 class LevelForm(forms.ModelForm):
     class Meta:
         model = Level
         fields = ['label']
+        widgets = {
+            'label' : forms.TextInput(
+                attrs={
+                    'id': 'label',
+                    'class': 'form-control',
+                    'name': 'label',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+        }
 
 class SemesterForm(forms.ModelForm):
     class Meta:
         model = Semester
         fields = ['title', 'level']
+        widgets = {
+            'title' : forms.TextInput(
+                attrs={
+                    'id': 'title',
+                    'class': 'form-control',
+                    'name': 'title',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+            'level': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+        }
 
 class ProgramForm(forms.ModelForm):
     class Meta:
         model = Program
         fields = ['title', 'description', 'program_date', 'person_in_charge', 'file']
+        widgets = {
+            'title' : forms.TextInput(
+                attrs={
+                    'id': 'title',
+                    'class': 'form-control',
+                    'name': 'title',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+            'program_date': forms.DateInput(
+                attrs={
+                    'type':'date',
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'description' : forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'description',
+                    'cols':"10",
+                    'rows':'5'
+                }
+            ),
+            'person_in_charge': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'file': forms.FileInput(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+        }
 
 class DocumentTypeForm(forms.ModelForm):
     class Meta:
@@ -67,26 +167,127 @@ class GroupSubjectForm(forms.ModelForm):
     class Meta:
         model = GroupSubject
         fields = ['title']
+        widgets = {
+            'title' : forms.TextInput(
+                attrs={
+                    'type':'text',
+                    'id': 'title',
+                    'class': 'form-control',
+                    'name': 'title',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+        }
 
 class SectorForm(forms.ModelForm):
     class Meta:
         model = Sector
         fields = ['title']
+        widgets = {
+            'title' : forms.TextInput(
+                attrs={
+                    'type':'text',
+                    'id': 'title',
+                    'class': 'form-control',
+                    'name': 'title',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+        }
 
 class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
         fields = ['label', 'sector', 'teacher_in_charge', 'level', 'type', 'subject_group', 'possible_evaluation', 'possible_averaging']
+        widgets = {
+            'label' : forms.TextInput(
+                attrs={
+                    'type':'text',
+                    'id': 'label',
+                    'class': 'form-control',
+                    'name': 'label',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+            'sector': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'teacher_in_charge': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'level': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'type': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'subject_group': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+            'possible_evaluation':forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input',
+                    'required': True,
+                }
+            ),
+            'possible_averaging':forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input',
+                    'required': True,
+                }
+            )
+        }
 
 class CareerForm(forms.ModelForm):
     class Meta:
         model = Career
         fields = ['title', 'sector']
+        widgets = {
+            'title' : forms.TextInput(
+                attrs={
+                    'type':'text',
+                    'id': 'title',
+                    'class': 'form-control',
+                    'name': 'title',
+                    'maxLength':'50',
+                    'placeholder': 'Titre du programmme',
+                    'required': True
+                }
+            ),
+            'sector': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'required': True,
+                }
+            ),
+        }
 
 class StudentCareerForm(forms.ModelForm):
     class Meta:
         model = StudentCareer
         fields = ['student', 'career', 'academic_year', 'semester']
+        
 
 class ScheduleForm(forms.ModelForm):
     class Meta:
