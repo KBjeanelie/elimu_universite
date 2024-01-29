a00d8c54e038823d7f773eb3b21f76268a5185e0
Jean-Elie <jeaneliekubembula@gmail.com>
2024-01-29 14:53:42 +0100

    Lister tous les emplois du temps et la fonction rechercher un emplois du temps

--- manager_dashboard/views/gestion_temps_view.py
+++ manager_dashboard/views/gestion_temps_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 
 from school_management.forms import ScheduleForm
+from school_management.models import Career, Schedule, Semester
 
 class AddScheduleView(View):
     template = 'manager_dashboard/gestion_temps/ajout_emplois_temps.html'
@@ -25,6 +26,39 @@ class AddScheduleView(View):
 
 class ScheduleView(View):
     template = 'manager_dashboard/gestion_temps/emplois_temps.html'
+    semesters = Semester.objects.all()
+    careers = Career.objects.all()
     
     def get(self, request, *args, **kwargs):
-        return render(request, template_name=self.template)
+        context = {
+            'semesters':self.semesters,
+            'careers':self.careers,
+        }
+        return render(request, template_name=self.template, context=context)
+    
+    def post(self, request, *args, **kwargs):
+        #semester_id = request.POST['semester']
+        career_id = request.POST['career']
+        career = Career.objects.get(pk=career_id)
+        
+        monday_schedule = Schedule.objects.filter(career__id=career_id, day='lundi').order_by('start_hours')
+        tueday_schedule = Schedule.objects.filter(career__id=career_id, day='mardi').order_by('start_hours')
+        wednesday_schedule = Schedule.objects.filter(career__id=career_id, day='mercredi').order_by('start_hours')
+        thursday_schedule = Schedule.objects.filter(career__id=career_id, day='jeudi').order_by('start_hours')
+        friday_schedule = Schedule.objects.filter(career__id=career_id, day='vendredi').order_by('start_hours')
+        saturday_schedule = Schedule.objects.filter(career__id=career_id, day='samedi').order_by('start_hours')
+        
+        context = {
+            'semesters':self.semesters,
+            'careers':self.careers,
+            'monday_schedule':monday_schedule,
+            'tueday_schedule':tueday_schedule,
+            'wednesday_schedule':wednesday_schedule,
+            'thursday_schedule':thursday_schedule,
+            'friday_schedule':friday_schedule,
+            'saturday_schedule':saturday_schedule,
+            'career':career
+        }
+        return render(request, template_name=self.template, context=context)
+        
+        return super().post(request, *args, **kwargs)
